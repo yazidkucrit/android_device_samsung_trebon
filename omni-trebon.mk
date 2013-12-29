1ba86bdda90c554a25bd1cef4e712d5b05bd241e
yazidkucrit <yazid.kucrit@gmail.com>
2013-12-30 02:58:28 +0700

    first commit

---
+++ omni-trebon.mk
@@ -0,0 +1,32 @@
+# Copyright (C) 2013 OmniROM Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+##Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
+
+## Inherit device files
+$(call inherit-product, device/samsung/trebon/trebon.mk)
+
+## Setup device configuration
+PRODUCT_NAME := omni_trebon
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := GT-S7500
+PRODUCT_RELEASE_NAME := GT-S7500
+PRODUCT_DEVICE := trebon
+
+## Bootanimation
+TARGET_SCREEN_HEIGHT := 480
+TARGET_SCREEN_WIDTH := 320
