f4397427108f63d763172002794f44d18a054d2a
yazidkucrit <yazid.kucrit@gmail.com>
2013-12-31 11:49:44 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -12,8 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH := $(call my-dir)
-
-ifeq ($(TARGET_DEVICE),trebon)
-include $(call all-makefiles-under,$(LOCAL_PATH))
-endif
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_trebon.mk 
